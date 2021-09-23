404d028c51ca281997663097a2f1607651e940f4
coleca24 <coleca@email.sc.edu>
2021-09-23 18:57:02 -0400

    Update SuperString.cpp

--- SuperString.cpp
+++ SuperString.cpp
@@ -26,3 +26,76 @@ int SuperString::length() {
 // DO NOT MODIFY END
 
 // PUT YOUR CODE BELOW!
+
+SuperString::SuperString() {
+}
+
+SuperString::SuperString(std::string str) {
+}
+
+SuperString::SuperString(int size, char val) {
+}
+
+SuperString::SuperString(const SuperString& copy) {
+}
+
+int SuperString::find(char c, int start) {
+    return -1;
+}
+
+int SuperString::find(std::string str, int start) {
+    return -1;
+}
+
+SuperString SuperString::substr(int start, int numChar) {
+    return *this;
+}
+
+SuperString SuperString::reverse() {
+    return *this;
+}
+
+SuperString SuperString::replace(int start, int numChar, std::string sub) {
+    return *this;
+}
+
+void SuperString::push_back(char c) {
+    return;
+}
+
+void SuperString::append(std::string str) {
+    return;
+}
+
+void SuperString::append(const SuperString& obj) {
+    return;
+}
+
+void SuperString::replace(char find, char rep) {
+    return;
+}
+
+void SuperString::removeAll(char c) {
+    return; 
+}
+
+// Bonus Methods
+void SuperString::replace(std::string find, std::string rep) {
+    return;
+}
+
+bool SuperString::isUpper() {
+    return true;
+}
+
+bool SuperString::isLower() {
+    return true;
+}
+
+bool SuperString::isTitleCase() {
+    return true;
+}
+
+SuperString::~SuperString() {
+    
+}
